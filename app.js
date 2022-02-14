9bef931624038610f13355db879ebc5bfb035bb1
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-15 05:48:11 +0600

    add calculate() function

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-document.getElementById('all-buttons').addEventListener('click', function(event){
-    const value = event.target.value;
-    const displayInput = document.getElementById('display-result');
-    const displayResult = displayInput.value;
-    displayInput.value = displayResult + value;
-
-    if(value == 'C'){
-        displayInput.value = '';
-    }
-})
+function calculate(x){
+    form.displayResult.value = form.displayResult.value + x;
+};
